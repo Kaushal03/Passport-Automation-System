88fa50c97c669119639c5aed5c93101d191edbe8
Kaushal Sethia <kaushalsethia01@gmail.com>
2021-11-13 14:15:40 +0530

    Delete hoverfunc.py

--- hoverfunc.py
+++
@@ -1,6 +0,0 @@
-
-
-def changeOnHover(button, colorOnHover, colorOnLeave):
-    button.bind("<Enter>", func=lambda e: button.config(background=colorOnHover))
-    button.bind("<Leave>", func=lambda e: button.config(background=colorOnLeave))
-
